e6f5d37e6b373ae7d74175318813714dc755a42b
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-10-06 04:03:19 +0200

    main-projects

---
+++ MVC - MovieRental/MovieRental/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MovieRental.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MovieRentalContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MovieRentalDb"),
+        b => b.MigrationsAssembly("MovieRental.DataAccess")));
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// 📌 Автоматски сеедирање на базата при старт
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<MovieRentalContext>();
+    DatabaseSeeder.Seed(context);
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
